a694ce555039a0992f84243c95a08a42bcb64d07
harith <hareth.cn12012@gmail.com>
2023-03-26 15:16:44 +0300

    completed

--- index.js
+++ index.js
@@ -27,14 +27,7 @@ function distanceFromHqInBlocks(currentLocation) {
     let distanceInFeet = Math.abs(startBlock - endBlock) * blockLength;
     return distanceInFeet;
   }
-
-
-//   calculatesFarePrice(start, destination)
-//       1) gives customers a free sample
-//       2) charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)
-//       3) charges 25 dollars for a distance over 2000 feet
-//       4) does not allow rides over 2500 feet
-  
+ 
 function calculatesFarePrice(start,end)
 {
     let price = 0 ;
